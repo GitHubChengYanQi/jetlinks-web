c113aaa53c8d9dc873e407a04e959c6225017d6b
Sing <78276478@qq.com>
2020-12-16 19:51:18 +0800

    修改本地端口

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
   home: {
     logLevel: 'info',
     component: 'ant',
-    baseURI: 'http://127.0.0.1:8081'
+    baseURI: 'http://localhost'
   },
   production: {
     logLevel: 'error'
