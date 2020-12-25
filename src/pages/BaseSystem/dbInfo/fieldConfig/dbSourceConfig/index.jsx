cf4eb90cb66488c7afd9b37215132d3c89d07e44
Sing <78276478@qq.com>
2020-12-25 17:22:01 +0800

    完善字段配置

--- src/pages/BaseSystem/dbInfo/fieldConfig/dbSourceConfig/index.jsx
+++ src/pages/BaseSystem/dbInfo/fieldConfig/dbSourceConfig/index.jsx
@@ -1,27 +1,63 @@
-import React from 'react';
-import {Button} from "antd";
+import React,{useState} from 'react';
+import {Button,Modal} from 'antd';
 
 const DbSourceConfig = ({value}) => {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   if (typeof value !== 'object') {
     return (<span>无需配置</span>);
   }
-  switch (value.type) {
-    case 'select':
-      return (
-        <>
-          <Button>选择数据接口</Button>
-        </>
-      );
-    case 'checkbox':
-      return (
-        <>
-          <Button>配置</Button>
-        </>
-      );
-    default:
-      return (<span>无需配置</span>);
-  }
+  const renderButton = () => {
+    switch (value.type) {
+      case 'select':
+        return (
+          <>
+            <Button>选择数据接口</Button>
+          </>
+        );
+      case 'checkbox':
+        return (
+          <>
+            <Button>配置</Button>
+          </>
+        );
+      default:
+        return (<span>无需配置</span>);
+    }
+  };
+
+  const renderForm = ()=>{
+    switch (value.type) {
+      case 'select':
+        return (
+          <>
+            <Button>选择数据接口</Button>
+
+          </>
+        );
+      case 'checkbox':
+        return (
+          <>
+            <Button>配置</Button>
+          </>
+        );
+      default:
+        return (<span>无配置</span>);
+    }
+  };
+
+  return (
+    <>
+      {renderButton()}
+      <Modal
+        visible={isModalVisible}
+      >
+        {renderForm()}
+      </Modal>
+    </>
+  );
+
 };
 
 export default DbSourceConfig;
