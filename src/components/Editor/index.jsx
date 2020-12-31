03ee0cdad19f0b8ef8a4a2363c6bab6bd8905136
Sing <78276478@qq.com>
2021-01-01 00:07:47 +0800

    编辑器增加图片上传

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, {useEffect, useImperativeHandle, useState} from 'react';
 import BraftEditor from 'braft-editor';
+import {ContentUtils} from 'braft-utils'
+import {Button, Upload} from 'antd';
+import {
+  FileImageOutlined
+} from '@ant-design/icons';
 import 'braft-editor/dist/index.css';
 
-const Editor = ({onChange,onBlur, value, ...props}) => {
+const Editor = ({onChange, onBlur, value, imgUploadProps, ...props}, ref) => {
 
-  const state = BraftEditor.createEditorState(value);
+  const [state, setState] = useState(null);
+
+  const imgUploadHandler = (object) => {
+    setState(
+      ContentUtils.insertMedias(state, [{
+        type: 'IMAGE',
+        url: URL.createObjectURL
+      }])
+    );
+  }
+
+  const extendControls = [];
+  if (imgUploadProps) {
+    extendControls.push({
+      key: 'antd-uploader',
+      type: 'component',
+      component: (
+        <Upload
+          accept="image/*"
+          showUploadList={false}
+          {...imgUploadProps}
+        >
+          <Button type="text" icon={<FileImageOutlined/>}>
+            插入图片
+          </Button>
+        </Upload>
+      )
+    });
+  }
+
+  useEffect(() => {
+    BraftEditor.createEditorState(value);
+  }, [value]);
+
+  useImperativeHandle(ref, () => ({
+      getState: () => {
+        return state;
+      },
+      ...ContentUtils
+    })
+  );
 
   return (
     <BraftEditor
@@ -14,9 +59,10 @@ const Editor = ({onChange,onBlur, value, ...props}) => {
       onBlur={(content) => {
         onChange(content.toHTML());
       }}
+      extendControls={extendControls}
       {...props}
     />
   );
 };
 
-export default Editor;
+export default React.forwardRef(Editor);
