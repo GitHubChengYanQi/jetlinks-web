9f4f4a59ed9e5ea3d868a66a8980bd851b83c287
siqiang <565902317@qq.com>
2021-08-07 16:44:57 +0800

    商机修改

--- src/pages/Erp/items/ItemsList/index.jsx
+++ src/pages/Erp/items/ItemsList/index.jsx
@@ -18,22 +18,30 @@ import ItemsEdit from '../ItemsEdit';
 import * as SysField from '../ItemsField';
 import Breadcrumb from '@/components/Breadcrumb';
 import Modal2 from '@/components/Modal';
+import CheckButton from "@/components/CheckButton";
 
 const {Column} = AntTable;
 const {FormItem} = Form;
 
-const ItemsList = () => {
+const ItemsList = (props) => {
   const ref = useRef(null);
   const tableRef = useRef(null);
-
+  const [ids, setIds] = useState([]);
+  const footer = () => {
+    /**
+     * 批量删除例子，根据实际情况修改接口地址
+     */
+    return (<DelButton api={{
+      url: '/',
+      method: 'POST'
+    }} value={ids}>批量删除</DelButton>);
+  };
   const actions = () => {
     return (
       <>
         <AddButton onClick={() => {
           ref.current.open(false);
         }} />
-
-
       </>
     );
   };
@@ -60,6 +68,11 @@ const ItemsList = () => {
         searchForm={searchForm}
         actions={actions()}
         ref={tableRef}
+        onChange={(keys)=>{
+          setIds(ids);
+          props.onChange(keys);
+        }}
+        footer={footer}
       >
         <Column title="产品名字" dataIndex="name" />
         <Column title="质保期" dataIndex="shelfLife" />
@@ -70,7 +83,6 @@ const ItemsList = () => {
         <Column title="材质" dataIndex="materialName" />
         <Column title="成本" dataIndex="cost" />
         <Column title="易损" dataIndex="vulnerability" />
-        <Column />
         <Column title="操作" align="right" render={(value, record) => {
           return (
             <>
