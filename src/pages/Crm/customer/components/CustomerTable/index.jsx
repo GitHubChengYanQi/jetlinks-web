197770ab53c17a3babd3b35d6d6caf87192c5293
Sing <78276478@qq.com>
2021-08-09 11:18:56 +0800

    调整客户列表
    增加客户等级标签

--- src/pages/Crm/customer/components/CustomerTable/index.jsx
+++ src/pages/Crm/customer/components/CustomerTable/index.jsx
@@ -22,13 +22,14 @@ import {useHistory} from 'ice';
 import CustomerEdit from '@/pages/Crm/customer/CustomerEdit';
 import Table from '@/components/Table';
 import BadgeState from '@/pages/Crm/customer/components/BadgeState';
+import CustomerLevel from '@/pages/Crm/customer/components/CustomerLevel';
 
 const {Column} = AntTable;
 const {FormItem} = Form;
 
 const CustomerTable = (props) => {
 
-  const {status, state,level} = props;
+  const {status, state, level} = props;
   const history = useHistory();
 
   const ref = useRef(null);
@@ -44,7 +45,7 @@ const CustomerTable = (props) => {
       tableRef.current.formActions.setFieldValue('customerLevelId', level ? level[0] : null);
       tableRef.current.submit();
     }
-  }, [status, state,level]);
+  }, [status, state, level]);
 
 
   const actions = () => {
@@ -74,14 +75,14 @@ const CustomerTable = (props) => {
      * 批量删除例子，根据实际情况修改接口地址
      */
     return (<DelButton api={{
-    ...customerBatchDelete
-    }} onSuccess={()=>{
+      ...customerBatchDelete
+    }} onSuccess={() => {
       tableRef.current.refresh();
     }} value={ids}>批量删除</DelButton>);
   };
 
   return (
-    <div id="listLayout" style={{height:'100%',overflowY:'auto'}}>
+    <div id="listLayout" style={{height: '100%', overflowY: 'auto'}}>
       <Table
         title={<Breadcrumb />}
         api={customerList}
@@ -93,9 +94,9 @@ const CustomerTable = (props) => {
         onChange={(keys) => {
           setIds(keys);
         }}
-        scroll={{x:'max-content' }}
+        scroll={{x: 'max-content'}}
         sticky={{
-          getContainer:() => {
+          getContainer: () => {
             console.log(document.getElementById('listLayout'));
             return document.getElementById('listLayout');
           }
@@ -108,35 +109,35 @@ const CustomerTable = (props) => {
             }}>{text}</Button>
           );
         }} />
-        <Column title="客户状态" width={120} render={(text, record) => {
-          return (
-            <BadgeState state={record.status} text={['潜在客户', '正式客户']} color={['red', 'green']} />
-          );
-        }} />
-        <Column title="客户级别" width={120}  render={(text,record)=>{
-          return (
-            <>
-              {record.crmCustomerLevelResult.level}
-            </>
-          );
-        }} />
-        <Column title="客户分类" width={120} dataIndex="classificationName" />
-        <Column title="公司类型" width={200} dataIndex="companyType" ellipsis />
-        <Column title="客户来源" width={120} render={(text,record)=>{
-          return (
-            <>
-              {record.originResult.originName}
-            </>
-          );
-        }} />
-        <Column title="负责人" width={120} render={(text,record)=>{
+        <Column title="负责人" width={200} render={(text, record) => {
           return (
             <>
               {record.userResult.account}
             </>
           );
         }} />
-        <Column title="行业" width={120} render={(text,record)=>{
+        <Column title="客户状态" width={140} align="center" render={(text, record) => {
+          return (
+            <BadgeState state={record.status} text={['潜在客户', '正式客户']} color={['red', 'green']} />
+          );
+        }} />
+        <Column title="客户级别" width={120} align="center" render={(text, record) => {
+          const level = typeof record.crmCustomerLevelResult === 'object' ? record.crmCustomerLevelResult : {};
+          return (
+            <CustomerLevel
+              level={level.rank}>{level.level}</CustomerLevel>);
+        }} />
+        <Column title="客户分类" width={120} align="center" dataIndex="classificationName" />
+        <Column title="公司类型" width={200} dataIndex="companyType" ellipsis />
+        <Column title="客户来源" width={120} render={(text, record) => {
+          return (
+            <>
+              {record.originResult.originName}
+            </>
+          );
+        }} />
+
+        <Column title="行业" width={120} render={(text, record) => {
           return (
             <>
               {record.crmIndustryResult.industryName}
@@ -149,9 +150,11 @@ const CustomerTable = (props) => {
               <EditButton onClick={() => {
                 ref.current.open(record.customerId);
               }} />
-              <DelButton api={{ url: '/customer/batchDelete',
+              <DelButton api={{
+                url: '/customer/batchDelete',
                 method: 'POST',
-                rowKey:'customerId'}} value={record.customerId} onSuccess={() => {
+                rowKey: 'customerId'
+              }} value={record.customerId} onSuccess={() => {
                 tableRef.current.refresh();
               }} />
             </>
