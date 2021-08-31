77f8cb2de18ddf1cd1ff422a62c28ff2cc74f711
weiChen <2683941980@qq.com>
2021-08-31 17:16:57 +0800

    添加微信绑定路径

--- src/pages/UserInfo/openUserInfo/components/User/index.jsx
+++ src/pages/UserInfo/openUserInfo/components/User/index.jsx
@@ -25,7 +25,7 @@ const User = () => {
             {
               data: {
                 userId: value,
-                page: ''
+                page: '/pages/bind/index'
               }
             }
           );
