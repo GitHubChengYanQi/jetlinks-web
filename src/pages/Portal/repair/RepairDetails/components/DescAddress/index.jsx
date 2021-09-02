20e884d0b5a8a5ccc681d52f413be1481fbe18c4
weiChen <2683941980@qq.com>
2021-09-02 18:10:17 +0800

    修改派工页面

--- src/pages/Portal/repair/RepairDetails/components/DescAddress/index.jsx
+++ src/pages/Portal/repair/RepairDetails/components/DescAddress/index.jsx
@@ -6,12 +6,14 @@ import ProSkeleton from '@ant-design/pro-skeleton';
 const DescAddress = (props) => {
   const {data} = props;
 
+  console.log(data);
+
 
   if (data) {
     return (
       <>
         <Descriptions>
-          <Descriptions.Item label="公司名称">{data.customerId ? data.customerId : '未填写'  }</Descriptions.Item>
+          <Descriptions.Item label="公司名称">{data.customerResult ? data.customerResult.customerName : '未填写'  }</Descriptions.Item>
           <Descriptions.Item label="省市区">{data.regionResult ?  `${data.regionResult[0].province}/${data.regionResult[0].city}/${data.regionResult[0].area}` : '未填写'}</Descriptions.Item>
           <Descriptions.Item label="详细地址">{data.address ? data.address : '未填写'}</Descriptions.Item>
           <Descriptions.Item label="报修人">{data.people ? data.people : '未填写'  }</Descriptions.Item>
