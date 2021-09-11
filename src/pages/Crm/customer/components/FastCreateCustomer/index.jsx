11f275b7ae9b947688ad2ef312b17cfd10892296
weiChen <2683941980@qq.com>
2021-09-11 15:32:43 +0800

    优化快速创建客户组件

--- src/pages/Crm/customer/components/FastCreateCustomer/index.jsx
+++ src/pages/Crm/customer/components/FastCreateCustomer/index.jsx
@@ -1,38 +1,50 @@
 import React, {useState} from 'react';
-import {message, Button, Card, Input} from 'antd';
+import {message, Button, Card, Input, Modal, Divider} from 'antd';
 import {useRequest} from '@/util/Request';
 import {customerAdd} from '@/pages/Crm/customer/CustomerUrl';
+import {CloseOutlined} from '@ant-design/icons';
+import style from './index.module.less';
 
-const FastCreateCustomer = ({close,add}) => {
+const FastCreateCustomer = ({close, add}) => {
 
-  const {run} = useRequest(customerAdd,{manual:true});
+  const {run} = useRequest(customerAdd, {manual: true});
 
-  const [value,setValue] = useState();
-  console.log(value);
+  const [value, setValue] = useState();
 
   return (
     <>
-      <Card tabList={[{tab:'输入客户名称',key:0}]} bordered={false}>
-        <Input onChange={(value)=>{
-          setValue(value.target.value);
-        }}/>
-        <Button type='link' onClick={async ()=>{
-          if (value){
-            const data = await run(
-              {
-                data:{
-                  customerName:value
-                }
-              }
-            );
-            typeof add === 'function' && add(data);
-          }else {
-            message.info('请输入客户名称！');
-          }
-        }}>保存</Button>
-        <Button type='link' onClick={()=>{
+      <Card
+        bodyStyle={{padding: '20px 0'}}
+        extra={  <Button type="link" style={{padding: 0, float: 'right', lineHeight: '100%'}} onClick={() => {
           typeof close === 'function' && close();
-        }}>取消</Button>
+        }}>
+          <CloseOutlined />
+        </Button>}
+        title={
+          <div>快速创建客户
+          </div>
+        }
+        headStyle={{textAlign: 'center'}} bordered={false}>
+        <Input placeholder="输入客户名称" onChange={(value) => {
+          setValue(value.target.value);
+        }} />
+        <Divider style={{margin: '20px 0'}} />
+        <div style={{textAlign: 'center'}}>
+          <Button type="link" onClick={async () => {
+            if (value) {
+              const data = await run(
+                {
+                  data: {
+                    customerName: value
+                  }
+                }
+              );
+              typeof add === 'function' && add(data);
+            } else {
+              message.info('请输入客户名称！');
+            }
+          }}>创建新客户</Button>
+        </div>
       </Card>
     </>
   );
