be212c5e40aaa6c35f214ffcf5f3f6474e52492f
sq <565902317@qq.com>
2021-09-27 16:55:13 +0800

    修改竞争

--- src/pages/Crm/competitor/competitorEdit/index.jsx
+++ src/pages/Crm/competitor/competitorEdit/index.jsx
@@ -66,28 +66,28 @@ const CompetitorEdit = ({onChange,...props}, ref) => {
               <FormItem label="竞争对手企业名称" name="name" dis={props.value || null} component={SysField.Name} required />
             </MegaLayout>
             <MegaLayout labelWidth={150} full>
-              <FormItem label="竞争项目名称" name="businessId" component={SysField.BusinessId} value={value && value.crmBusinessList && value.crmBusinessList.length>0 && value.crmBusinessList[0].businessId} required />
+              <FormItem label="竞争项目名称" name="businessId" component={SysField.BusinessId} value={value && value.crmBusinessList && value.crmBusinessList.length>0 && value.crmBusinessList[0].businessId}  />
             </MegaLayout>
             <MegaLayout labelWidth={150}>
               <FormItem label="地区" name="region" component={SysField.Region} required />
             </MegaLayout>
             <MegaLayout labelWidth={150} grid>
               <FormItem label="创立日期" name="creationDate" component={SysField.CreationDate} required />
-              <FormItem label="联系电话" name="phone" component={SysField.Phone} rules={[{
-                message: '请输入正确的手机号码!',
-                pattern: /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/
-              }]} />
+              {/*<FormItem label="联系电话" name="phone" component={SysField.Phone} rules={[{*/}
+              {/*  message: '请输入正确的手机号码!',*/}
+              {/*  pattern: /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/*/}
+              {/*}]} />*/}
             </MegaLayout>
+            {/*<MegaLayout labelWidth={150} grid>*/}
+            {/*  <FormItem label="网址 " name="url" component={SysField.Url} rules={[{required:true,message:'请输入正确的网址',pattern:'^(http(s)?:\\/\\/)?(www\\.)?[\\w-]+\\.(com|net|cn)$'}]} />*/}
+            {/*  <FormItem label="邮箱" name="email" component={SysField.Email} rules={[{required:true,message:'请输入正确的邮箱',pattern:'^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z0-9]{2,6}$'}]} />*/}
+            {/*</MegaLayout>*/}
+            {/*<MegaLayout labelWidth={150} grid>*/}
+            {/*  <FormItem label="员工规模" name="staffSize" component={SysField.StaffSize} required />*/}
+            {/*  <FormItem label="公司所有制" name="ownership" component={SysField.Ownership} required />*/}
+            {/*</MegaLayout>*/}
             <MegaLayout labelWidth={150} grid>
-              <FormItem label="网址 " name="url" component={SysField.Url} rules={[{required:true,message:'请输入正确的网址',pattern:'^(http(s)?:\\/\\/)?(www\\.)?[\\w-]+\\.(com|net|cn)$'}]} />
-              <FormItem label="邮箱" name="email" component={SysField.Email} rules={[{required:true,message:'请输入正确的邮箱',pattern:'^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z0-9]{2,6}$'}]} />
-            </MegaLayout>
-            <MegaLayout labelWidth={150} grid>
-              <FormItem label="员工规模" name="staffSize" component={SysField.StaffSize} required />
-              <FormItem label="公司所有制" name="ownership" component={SysField.Ownership} required />
-            </MegaLayout>
-            <MegaLayout labelWidth={150} grid>
-              <FormItem label="竞争级别" name="competitionLevel" component={SysField.CompetitionLevel} required />
+              <FormItem label="竞争级别" name="competitionLevel" component={SysField.CompetitionLevel}  />
               <FormItem label="年销售" name="annualSales" component={SysField.AnnualSales} required />
             </MegaLayout>
             <MegaLayout labelWidth={150}>
