76e6141657cedd322528bec04eb5a3f4ff796ad2
weiChen <2683941980@qq.com>
2021-11-15 16:04:00 +0800

    修改生产设置

--- src/pages/PRODUCTION/router/index.jsx
+++ src/pages/PRODUCTION/router/index.jsx
@@ -1,33 +1,14 @@
-
-
-
-import {MaterialRouter} from '@/pages/Erp/material/MaterialRouter';
-import {ItemsRouter} from '@/pages/Erp/items/ItemsRouter';
-import {BrandRouter} from '@/pages/Erp/brand/BrandRouter';
-import {PartsRouter} from '@/pages/Erp/parts/PartsRouter';
-import { StorehouseRouter} from '@/pages/Erp/storehouse/StorehouseRouter';
-import {StockRouter} from '@/pages/Erp/stock/StockRouter';
-import {InstockRouter} from '@/pages/Erp/instock/InstockRouter';
-import {OutstockRouter} from '@/pages/Erp/outstock/OutstockRouter';
-import ErpLayout from '@/pages/Erp';
-import {OrderBranchRouter} from '@/pages/Erp/orderBranch/OrderBranchRouter';
-import {OrderRouter} from '@/pages/Erp/order/OrderRouter';
-import {PackageRouter} from '@/pages/Erp/package/packageRouter';
-import {DeliveryRouter} from '@/pages/Erp/delivery/deliveryRouter';
-import {SpuRouter} from '@/pages/Erp/spu/spuRouter';
-import {ProductOrderRouter} from '@/pages/Erp/productOrder/productOrderRouter';
 import {ToolRouter} from '@/pages/Erp/tool/toolRouter';
 import {QualityCheckRouter} from '@/pages/Erp/qualityCheck/qualityCheckRouter';
 import SpuLayout from '@/pages/SPU';
-import {SkuRouter} from '@/pages/Erp/sku/skuRouter';
-import {SPUSRouter} from '@/pages/Erp/Spus/spuRouter';
 import {QrCodeRouter} from '@/pages/Erp/qrCode/qrCodeRouter';
+import ProducttionLayout from '@/pages/Production';
 
 const ProductionRouterConfig = [
   {
     path: '/production',
     name: '产品物料',
-    component: SpuLayout,
+    component: ProducttionLayout,
     children:[
       ...ToolRouter,
       ...QualityCheckRouter,
