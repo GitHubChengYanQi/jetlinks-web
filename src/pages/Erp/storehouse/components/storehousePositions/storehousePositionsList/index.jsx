5461cb90a95217e62729f0bd666dd2dd6148d999
weiChen <2683941980@qq.com>
2022-01-06 09:13:03 +0800

    修改库位查询

--- src/pages/Erp/storehouse/components/storehousePositions/storehousePositionsList/index.jsx
+++ src/pages/Erp/storehouse/components/storehousePositions/storehousePositionsList/index.jsx
@@ -5,34 +5,28 @@
  * @Date 2021-10-29 09:22:25
  */
 
-import React, {useRef} from 'react';
-import Table from '@/components/Table';
-import {Table as AntTable} from 'antd';
+import React, {useRef, useState} from 'react';
+import { SearchOutlined} from '@ant-design/icons';
+import {Button, Card, Divider, Input, Space, Table as AntTable} from 'antd';
 import DelButton from '@/components/DelButton';
 import Drawer from '@/components/Drawer';
 import AddButton from '@/components/AddButton';
 import EditButton from '@/components/EditButton';
-import Form from '@/components/Form';
 import {
   storehousePositionsDelete,
-  storehousePositionsList,
-  storehousePositionsTreeView
 } from '../storehousePositionsUrl';
 import StorehousePositionsEdit from '../storehousePositionsEdit';
-import * as SysField from '../storehousePositionsField';
 import Breadcrumb from '@/components/Breadcrumb';
-import {createFormActions} from '@formily/antd';
 import Code from '@/pages/Erp/spu/components/Code';
-import {ScanOutlined} from '@ant-design/icons';
+import {useRequest} from '@/util/Request';
 
 const {Column} = AntTable;
-const {FormItem} = Form;
 
 const StorehousePositionsList = (props) => {
-  const formActionsPublic = createFormActions();
   const {value} = props;
   const ref = useRef(null);
   const tableRef = useRef(null);
+  const [name,setName] = useState();
   const actions = () => {
     return (
       <>
@@ -43,64 +37,62 @@ const StorehousePositionsList = (props) => {
     );
   };
 
-  const searchForm = () => {
-    return (
-      <>
-        <FormItem hidden name="storehouseId" value={value} component={SysField.StorehouseId} />
-        <FormItem hidden name="name" component={SysField.Name} />
-      </>
-    );
-  };
+  const {loading, data, run, refresh} = useRequest({
+    url: `/storehousePositions/treeView?ids=${value}`,
+    method: 'GET',
+  },);
 
   return (
     <>
-      <Table
-        title={<Breadcrumb title='仓库库位' />}
-        api={{
-          url: `/storehousePositions/treeView?ids=${value}`,
-          method: 'GET',
-        }}
-        rowKey="value"
-        pagination={false}
-        footer={false}
-        formActions={formActionsPublic}
-        SearchButton
-        headStyle={{margin:0,padding:0}}
-        rowSelection
-        noSort
-        contentHeight
-        searchForm={searchForm}
-        actions={actions()}
-        ref={tableRef}
-      >
-        <Column title="库位名称" width={200} dataIndex="label" render={(value,record)=>{
-          if (record.children && record.children.length === 0){
-            record.children = null;
-          }
-          return (
-            <>
-              <Code style={{width:24,height:24}} source='storehousePositions' id={record.value} />
-              {value}
-            </>
-          );
-        }} />
-        <Column title="操作" align="right" render={(value, record) => {
-          if (record.value !== '0'){
+      <Card title={<Breadcrumb title="仓库库位" />} extra={actions()}>
+        <Space>
+          <Input placeholder="库位名称" onChange={(value)=>{
+            setName(value.target.value);
+          }} />
+          <Button type="primary" onClick={() => {
+            run({
+              params: {
+                name,
+              }
+            });
+          }}><SearchOutlined />查询</Button>
+        </Space>
+        <Divider />
+        <AntTable
+          dataSource={data || []}
+          loading={loading}
+          rowKey="value"
+          pagination={false}
+        >
+          <Column title="库位名称" width={200} dataIndex="label" render={(value, record) => {
+            if (record.children && record.children.length === 0) {
+              record.children = null;
+            }
             return (
               <>
-                <EditButton onClick={() => {
-                  ref.current.open(record.value);
-                }} />
-                <DelButton api={storehousePositionsDelete} value={record.value} onSuccess={() => {
-                  tableRef.current.refresh();
-                }} />
+                <Code style={{width: 24, height: 24}} source="storehousePositions" id={record.value} />
+                {value}
               </>
             );
-          }
-        }} width={100} />
-      </Table>
+          }} />
+          <Column title="操作" align="right" render={(value, record) => {
+            if (record.value !== '0') {
+              return (
+                <>
+                  <EditButton onClick={() => {
+                    ref.current.open(record.value);
+                  }} />
+                  <DelButton api={storehousePositionsDelete} value={record.value} onSuccess={() => {
+                    tableRef.current.refresh();
+                  }} />
+                </>
+              );
+            }
+          }} width={100} />
+        </AntTable>
+      </Card>
       <Drawer width={800} title="编辑" component={StorehousePositionsEdit} onSuccess={() => {
-        tableRef.current.refresh();
+        refresh();
         ref.current.close();
       }} ref={ref} storehouse={value} />
     </>
