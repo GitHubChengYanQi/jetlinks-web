5205d4716e65ea0cf83cf0dfe506b7f7a5994dc7
weiChen <2683941980@qq.com>
2022-04-27 13:38:22 +0800

    修改处理按钮样式

--- src/pages/Erp/Analysis/TaskList/components/SkuImport/index.jsx
+++ src/pages/Erp/Analysis/TaskList/components/SkuImport/index.jsx
@@ -1,5 +1,6 @@
 import React, {useImperativeHandle, useRef, useState} from 'react';
-import {Alert, Button, Input, notification, Space, Spin, Table as AntTable} from 'antd';
+import {Alert, Avatar, Button, Dropdown, Input, Menu, Space, Spin, Table as AntTable} from 'antd';
+import {DownOutlined} from '@ant-design/icons';
 import Table from '@/components/Table';
 import Form from '@/components/Form';
 import {useRequest} from '@/util/Request';
@@ -7,6 +8,8 @@ import Empty from '@/components/Empty';
 import Modal from '@/components/Modal';
 import AddSkuModal from '@/pages/Erp/sku/SkuTable/AddSkuModal';
 import TaskProgress from '@/pages/Erp/Analysis/TaskList/components/TaskProgress';
+import styles from '@/layouts/BasicLayout/components/Header/index.module.less';
+import {config} from 'ice';
 
 const {Column} = AntTable;
 const {FormItem} = Form;
@@ -87,7 +90,7 @@ const SkuImport = ({...props}, ref) => {
       return <Empty />;
     }
     if (!load) {
-      return <TaskProgress data={data} title='导入中' />;
+      return <TaskProgress data={data} title="导入中" />;
     } else {
       return <div style={{maxWidth: 1700}}>
         <Table
@@ -161,7 +164,7 @@ const SkuImport = ({...props}, ref) => {
     <Modal
       ref={modalRef}
       headTitle={
-        load? <Space>
+        load ? <Space>
           <div>
             导入成功 <Button type="link" style={{padding: 0}}>{data && data.successNum}</Button> 条
           </div>
@@ -173,112 +176,120 @@ const SkuImport = ({...props}, ref) => {
       }
       width="auto"
       footer={load && [
-        <Button
-          key="0"
-          type="primary"
-          ghost
-          disabled={
-            selectedRows.length !== 1
+        <Dropdown
+          key={1}
+          overlay={
+            <Menu>
+              <Menu.Item
+                key="0"
+                disabled={
+                  selectedRows.length !== 1 || !disabled
+                }
+                onClick={() => {
+                  const row = selectedRows[0];
+                  const skuResult = row.simpleResult || {};
+                  const describe = row.describe;
+                  setErrKeys([row.detailId]);
+                  addRef.current.open({
+                    defaultValue: {
+                      spuClass: row.classId,
+                      spu: {id: skuResult.spuId, name: row.classItem},
+                      skuName: row.skuName,
+                      unitId: row.unitId,
+                      standard: row.standard,
+                      batch: row.isNotBatch === '是' ? 1 : 0,
+                      specifications: row.specifications,
+                      sku: describe && describe.split(',').map((item) => {
+                        return {
+                          label: item.split(':')[0],
+                          value: item.split(':')[1],
+                        };
+                      }),
+                    }
+                  });
+                }}
+              >
+                处理
+              </Menu.Item>
+              <Menu.Item
+                key="1"
+                disabled={selectedRows.length !== 1 || disabled}
+                onClick={() => {
+                  const row = selectedRows[0];
+                  const skuData = row.simpleResult || {};
+
+                  const describe = [];
+                  row.describe && row.describe.split(',').map((item) => {
+                    return describe.push({
+                      label: item.split(':')[0],
+                      value: item.split(':')[1],
+                    });
+                  });
+
+                  skuData.list.map((item) => {
+                    return describe.push({
+                      label: item.itemAttributeResult && item.itemAttributeResult.attribute,
+                      value: item.attributeValues,
+                    });
+                  });
+                  setErrKeys([row.detailId]);
+                  addRef.current.open({
+                    errKey: row.detailId,
+                    ...skuData,
+                    specifications: row.specifications || skuData.specifications,
+                    newCoding: row.standard,
+                    merge: true,
+                    skuJsons: [],
+                    defaultValue: {
+                      sku: describe
+                    }
+                  });
+                }}
+              >
+                合并
+              </Menu.Item>
+              <Menu.Item
+                key="2"
+                disabled={selectedRows.length === 0 || disabled || codingDisabled}
+                onClick={() => {
+                  setErrKeys(selectedRows.map(item => item.detailId));
+                  addSkuRef.current.batchRun({
+                    data: {
+                      skuParams: selectedRows.map((item) => {
+                        const skuResult = item.simpleResult || {};
+                        const spuResult = skuResult.spuResult || {};
+                        const describe = item.describe;
+
+                        return {
+                          standard: item.standard,
+                          spuClass: skuResult.spuClass,
+                          spu: {id: skuResult.spuId, name: spuResult.name},
+                          skuName: skuResult.skuName,
+                          unitId: spuResult.unitId,
+                          batch: item.isNotBatch === '是' ? 1 : 0,
+                          specifications: item.specifications,
+                          sku: describe && describe.split(',').map((item) => {
+                            return {
+                              label: item.split(':')[0],
+                              value: item.split(':')[1],
+                            };
+                          }),
+                        };
+                      })
+                    }
+                  });
+                }}
+              >
+                直接导入
+              </Menu.Item>
+            </Menu>
           }
-          onClick={() => {
-            const row = selectedRows[0];
-            const skuResult = row.simpleResult || {};
-            const describe = row.describe;
-            setErrKeys([row.detailId]);
-            addRef.current.open({
-              defaultValue: {
-                spuClass: row.classId,
-                spu: {id: skuResult.spuId, name: row.classItem},
-                skuName: row.skuName,
-                unitId: row.unitId,
-                standard: row.standard,
-                batch: row.isNotBatch === '是' ? 1 : 0,
-                specifications: row.specifications,
-                sku: describe && describe.split(',').map((item) => {
-                  return {
-                    label: item.split(':')[0],
-                    value: item.split(':')[1],
-                  };
-                }),
-              }
-            });
-          }}>
-          处理
-        </Button>,
-        <Button
-          key="2"
-          disabled={selectedRows.length !== 1 || disabled}
-          type="primary"
-          ghost
-          onClick={() => {
-            const row = selectedRows[0];
-            const skuData = row.simpleResult || {};
-
-            const describe = [];
-            row.describe && row.describe.split(',').map((item) => {
-              return describe.push({
-                label: item.split(':')[0],
-                value: item.split(':')[1],
-              });
-            });
-
-            skuData.list.map((item) => {
-              return describe.push({
-                label: item.itemAttributeResult && item.itemAttributeResult.attribute,
-                value: item.attributeValues,
-              });
-            });
-            setErrKeys([row.detailId]);
-            addRef.current.open({
-              errKey: row.detailId,
-              ...skuData,
-              specifications:row.specifications || skuData.specifications,
-              newCoding: row.standard,
-              merge: true,
-              skuJsons: [],
-              defaultValue: {
-                sku: describe
-              }
-            });
-          }}>
-          合并
-        </Button>,
-        <Button
-          key="1"
-          disabled={selectedRows.length === 0 || disabled || codingDisabled}
-          type="primary"
-          ghost
-          onClick={() => {
-            setErrKeys(selectedRows.map(item => item.detailId));
-            addSkuRef.current.batchRun({
-              data: {
-                skuParams: selectedRows.map((item) => {
-                  const skuResult = item.simpleResult || {};
-                  const spuResult = skuResult.spuResult || {};
-                  const describe = item.describe;
-
-                  return {
-                    standard: item.standard,
-                    spuClass: skuResult.spuClass,
-                    spu: {id: skuResult.spuId, name: spuResult.name},
-                    skuName: skuResult.skuName,
-                    unitId: spuResult.unitId,
-                    batch: item.isNotBatch === '是' ? 1 : 0,
-                    specifications: item.specifications,
-                    sku: describe && describe.split(',').map((item) => {
-                      return {
-                        label: item.split(':')[0],
-                        value: item.split(':')[1],
-                      };
-                    }),
-                  };
-                })
-              }
-            });
-          }}>
-          直接导入
-        </Button>,
-      ]}
+          trigger={['click']}
+        >
+          <Button type="primary" key={1}>处理 <DownOutlined /></Button>
+        </Dropdown>
+      ]
+      }
     >
       {
         loading
