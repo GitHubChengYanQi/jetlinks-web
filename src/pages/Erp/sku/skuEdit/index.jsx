48c446447d5b9bb8cb65609f3cab324d5593f00a
Genius <2683941980@qq.com>
2022-09-02 08:57:05 +0800

    型号规格

--- src/pages/Erp/sku/skuEdit/index.jsx
+++ src/pages/Erp/sku/skuEdit/index.jsx
@@ -199,7 +199,8 @@ const SkuEdit = ({...props}, ref) => {
           component={SysField.SpuCoding}
         />
         <FormItem
-          label="型号/规格"
+          // label="型号/规格"
+          label='型号'
           name="skuName"
           component={SysField.SkuName}
           required />
@@ -215,7 +216,8 @@ const SkuEdit = ({...props}, ref) => {
           required
         />
         <FormItem
-          label="执行标准"
+          // label="执行标准"
+          label='规格'
           placeholder="无规格内容可填写“型号”"
           name="specifications"
           component={SysField.Specs}
