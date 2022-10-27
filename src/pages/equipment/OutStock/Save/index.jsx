ff02a9afb22fde3fe5663e6138b6c8a596d990ae
cheng <2683941980@qq.com>
2022-10-27 16:58:51 +0800

    出库接口

--- src/pages/equipment/OutStock/Save/index.jsx
+++ src/pages/equipment/OutStock/Save/index.jsx
@@ -5,6 +5,7 @@ import {outstockAdd, outstockEdit,} from '@/pages/equipment/OutStock/url';
 import DatePicker from '@/components/DatePicker';
 import SelectCustomer from '@/pages/equipment/OutStock/Save/components/SelectCustomer';
 import InputNumber from '@/components/InputNumber';
+import {isArray} from '@/util/Tools';
 
 
 const Save = ({success, close, visible}) => {
@@ -19,6 +20,17 @@ const Save = ({success, close, visible}) => {
     success={success}
     visible={visible}
     close={close}
+    format={(values) => {
+
+      return {
+        customerId: values.customerId,
+        params: isArray(visible).map(item => ({
+          deviceId: item.deviceId,
+          warranty: values.warranty,
+          outstockTime: values.outstockTime,
+        }))
+      };
+    }}
   >
     <Form.Item
       key="customerId"
@@ -28,7 +40,7 @@ const Save = ({success, close, visible}) => {
         {required: true, message: '请选择所属客户'},
       ]}
     >
-      <SelectCustomer/>
+      <SelectCustomer />
     </Form.Item>
     <Form.Item
       key="warranty"
@@ -38,7 +50,7 @@ const Save = ({success, close, visible}) => {
         {required: true, message: '请输入质保时长'},
       ]}
     >
-      <InputNumber addonAfter="月" placeholder="请输入质保时长"/>
+      <InputNumber addonAfter="月" placeholder="请输入质保时长" />
     </Form.Item>
     <Form.Item
       key="outstockTime"
@@ -48,7 +60,7 @@ const Save = ({success, close, visible}) => {
         {required: true, message: '请选择出库时间'},
       ]}
     >
-      <DatePicker placeholder="请选择出库时间"/>
+      <DatePicker placeholder="请选择出库时间" />
     </Form.Item>
   </AntForm>;
 };
